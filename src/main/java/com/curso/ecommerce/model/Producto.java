596b5ec0772fe1ec47d6832914253d62f9503b3c
francogaliotti <francogaliotti@gmail.com>
2022-11-22 17:30:17 -0300

    first commit

---
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -0,0 +1,47 @@
+package com.curso.ecommerce.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Audited
+public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+	private String imagen;
+	private double precio;
+	private int cantidad;
+	
+	@ManyToOne
+	private Usuario usuario;
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
+				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
+	}
+	
+	
+}
